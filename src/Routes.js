566f5f0e1a85b385225f2135bf7227fced33b3c2
Arnab Khan <khanarnab563@gmail.com>
2021-10-04 16:49:39 -0500

    commit on deleted stuff

--- src/Routes.js
+++ src/Routes.js
@@ -15,7 +15,6 @@ import { LeftSidebar, PresentationLayout } from './layout-blueprints';
 import Buttons from './example-pages/Buttons';
 import Dropdowns from './example-pages/Dropdowns';
 import NavigationMenus from './example-pages/NavigationMenus';
-import ProgressBars from './example-pages/ProgressBars';
 import Pagination from './example-pages/Pagination';
 import Scrollable from './example-pages/Scrollable';
 import Badges from './example-pages/Badges';
@@ -26,15 +25,10 @@ import RegularTables4 from './example-pages/RegularTables4';
 import FormsControls from './example-pages/FormsControls';
 
 const DashboardDefault = lazy(() => import('./example-pages/DashboardDefault'));
-const Cards3 = lazy(() => import('./example-pages/Cards3'));
 const LandingPage = lazy(() => import('./example-pages/LandingPage'));
-const Accordions = lazy(() => import('./example-pages/Accordions'));
-const Modals = lazy(() => import('./example-pages/Modals'));
 const Notifications = lazy(() => import('./example-pages/Notifications'));
 const Popovers = lazy(() => import('./example-pages/Popovers'));
 const Tabs = lazy(() => import('./example-pages/Tabs'));
-const ApexCharts = lazy(() => import('./example-pages/ApexCharts'));
-const Maps = lazy(() => import('./example-pages/Maps'));
 const ListGroups = lazy(() => import('./example-pages/ListGroups'));
 
 const Routes = () => {
@@ -95,23 +89,17 @@ const Routes = () => {
                 '/Buttons',
                 '/Dropdowns',
                 '/NavigationMenus',
-                '/ProgressBars',
                 '/Pagination',
                 '/Scrollable',
                 '/Badges',
                 '/Icons',
                 '/UtilitiesHelpers',
-                '/Cards3',
-                '/Accordions',
-                '/Modals',
                 '/Notifications',
                 '/Popovers',
                 '/Tabs',
                 '/RegularTables1',
                 '/RegularTables4',
                 '/FormsControls',
-                '/ApexCharts',
-                '/Maps',
                 '/ListGroups'
               ]}>
               <LeftSidebar>
@@ -132,7 +120,6 @@ const Routes = () => {
                       path="/NavigationMenus"
                       component={NavigationMenus}
                     />
-                    <Route path="/ProgressBars" component={ProgressBars} />
                     <Route path="/Pagination" component={Pagination} />
                     <Route path="/Scrollable" component={Scrollable} />
                     <Route path="/Badges" component={Badges} />
@@ -141,17 +128,12 @@ const Routes = () => {
                       path="/UtilitiesHelpers"
                       component={UtilitiesHelpers}
                     />
-                    <Route path="/Cards3" component={Cards3} />
-                    <Route path="/Accordions" component={Accordions} />
-                    <Route path="/Modals" component={Modals} />
                     <Route path="/Notifications" component={Notifications} />
                     <Route path="/Popovers" component={Popovers} />
                     <Route path="/Tabs" component={Tabs} />
                     <Route path="/RegularTables1" component={RegularTables1} />
                     <Route path="/RegularTables4" component={RegularTables4} />
                     <Route path="/FormsControls" component={FormsControls} />
-                    <Route path="/ApexCharts" component={ApexCharts} />
-                    <Route path="/Maps" component={Maps} />
                     <Route path="/ListGroups" component={ListGroups} />
                   </motion.div>
                 </Switch>
