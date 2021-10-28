6cf40d890fa9b1c60fe1dafa9e2d97f3e222f3c1
jaimecardosom <jaimemcardoso@yahoo.com>
2021-10-28 15:38:34 -0500

    sidebar fixed

--- src/example-components/DashboardDefault/DashboardDefaultSection1/Sidebar.js
+++ src/example-components/DashboardDefault/DashboardDefaultSection1/Sidebar.js
@@ -3,6 +3,8 @@ import React from 'react';
 export default () => {
   const onDragStart = (event, nodeType) => {
     event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.setData("nodeName", event.target.firstChild.nodeValue);
+    event.dataTransfer.setData("className", event.target.className);
     event.dataTransfer.effectAllowed = 'move';
   };
 
@@ -12,26 +14,14 @@ export default () => {
         You can drag these nodes to the pane on the right. Delete nodes using
         'Backspace'.
       </div>
-      <div className="diamond" onDragStart={(event) => onDragStart(event, 'diamond')} draggable>
-      </div>
-      <div className="dndnode one" onDragStart={(event) => onDragStart(event, 'Unifier')} draggable>
-        Unifier
-      </div>
-      <div className="dndnode two" onDragStart={(event) => onDragStart(event, 'P6')} draggable>
-        P6
-      </div>
-      <div className="dndnode three" onDragStart={(event) => onDragStart(event, 'SQL')} draggable>
-        SQL
-      </div>
-      <div className="dndnode four" onDragStart={(event) => onDragStart(event, 'FTP')} draggable>
+      <div className="dndnode FTP"
+        onDragStart={(event) => onDragStart(event, "FTP")}
+        draggable
+      >
         FTP
       </div>
-      <div className="dndnode five" onDragStart={(event) => onDragStart(event, 'CSV')} draggable>
-        CSV
-      </div>
-      <div className="dndnode six" onDragStart={(event) => onDragStart(event, 'Email')} draggable>
-        Email
-      </div>
+
+      
     </aside>
   );
 };
--- src/example-components/DashboardDefault/DashboardDefaultSection1/index.js
+++ src/example-components/DashboardDefault/DashboardDefaultSection1/index.js
@@ -1,17 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useRef } from "react";
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  removeElements,
+  Controls
+} from "react-flow-renderer";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
+import FTP from "./Ftp";
+import Sidebar from "./Sidebar";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const initialElements = [
+  {
+    id: '1',
+    type: 'input',
+    className: "dndnode input",
+    data: { label: 'Start' },
+    
+    position: { x: 250, y: 5 },
+
+  },
+  {
+    id: '2',
+    className: "dndnode output",
+    type: 'output',
+    data: { label: 'End' },
+    position: { x: 500, y: 10 }
+  }
+
+
+];
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
+export default function App() {
+  const nodeTypes = {
+
+    mirror: FTP,
+
+  };
+  const reactFlowWrapper = useRef(null);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  const [elements, setElements] = useState(initialElements);
+  const onConnect = (params) => setElements((els) => addEdge(params, els));
+  const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+  const onLoad = (_reactFlowInstance) =>
+    setReactFlowInstance(_reactFlowInstance);
+  const onDragOver = (event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  };
+  const onDrop = (event) => {
+    event.preventDefault();
+    const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+    const type = event.dataTransfer.getData("application/reactflow");
+    const name = event.dataTransfer.getData("nodeName");
+    const className = event.dataTransfer.getData("className");
+    const position = reactFlowInstance.project({
+      x: event.clientX - reactFlowBounds.left,
+      y: event.clientY - reactFlowBounds.top
+    });
+    const newNode = {
+      id: getId(),
+      type,
+      position,
+      className: className,
+      data: { label: name }
+    };
+    setElements((es) => es.concat(newNode));
+  };
+  return (
+    <div className="dndflow">
+      <ReactFlowProvider>
+        <div
+          className="reactflow-wrapper"
+          style={{ height: "500px", width: "500px" }}
+          ref={reactFlowWrapper}
+        >
+          <ReactFlow
+            elements={elements}
+            onConnect={onConnect}
+            nodeTypes={nodeTypes}
+            onElementsRemove={onElementsRemove}
+            onLoad={onLoad}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+          >
+            <Controls />
+          </ReactFlow>
+        </div>
+        <Sidebar />
+      </ReactFlowProvider>
+    </div>
+  );
+}
